8a51a7b45ff12b8f26cde9da4195904d3f8ff65f
thanhkute <thanhpt2612@gmail.com>
2022-03-08 15:16:31 +0700

    Blog NodeJS

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+const route = require('./routes');
+
+//http logger
+app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// template engine
+app.engine('hbs', handlebars({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+// routes init
+route(app)
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
